48d65892f4ab3921f65bd26c1fc3fb5a8f7d174a
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-03-15 10:18:08 +0100

    Gestione Textbox
    Implementata funzione per barrare e rimuovere note

--- src/renderer.ts
+++ src/renderer.ts
@@ -14,7 +14,48 @@ const notes = <HTMLDivElement>document.getElementById("note");
 textobx.addEventListener('keypress', (event: KeyboardEvent) => {
     if (event.key === 'Enter') {
         console.log(textobx.value);
-        notes.innerHTML += `<span>${textobx.value}</span>`;
+
+        addNote(textobx.value);
         textobx.value = '';
     }
 });
+
+
+function addNote(note: string): void {
+    const newNote = document.createElement('span');
+    newNote.innerText = note;
+
+    // for right click
+    newNote.addEventListener('contextmenu', (event: MouseEvent) => {
+        manageNote(newNote, event);
+    })
+
+    // for left click
+    newNote.addEventListener('click', (event: MouseEvent) => {
+        manageNote(newNote, event);
+    })
+
+    notes.appendChild(newNote);
+}
+
+// potrebbe essere spostata in addNote
+function manageNote(note: HTMLSpanElement, event: MouseEvent): void {
+    console.log('Managing note');
+    console.log(note.textContent);
+
+    switch (event.button) {
+        case 0:
+            console.log('Left click');
+            note.classList.toggle('barred');
+
+            break;
+        case 1:
+            console.log('Middle click');
+            break;
+        case 2:
+            console.log('Right click');
+            notes.removeChild(note);
+            
+            break;
+    }
+}
