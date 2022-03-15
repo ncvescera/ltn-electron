7d5b91598e8fe6120066acac5a7c0ae8529897de
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-03-15 09:38:07 +0100

    Preload.ts
    Eliminati ! e sostituiti con casting (?)

--- src/preload.ts
+++ src/preload.ts
@@ -13,10 +13,10 @@ window.addEventListener("DOMContentLoaded", () => {
     //}
     // ------------//
     
-    const minButton = document.getElementById('min-button')!;
-    const maxButton = document.getElementById('max-button')!;
-    const restoreButton = document.getElementById('restore-button')!;
-    const closeButton = document.getElementById('close-button')!;
+    const minButton = <HTMLButtonElement>document.getElementById('min-button');
+    const maxButton = <HTMLButtonElement>document.getElementById('max-button');
+    const restoreButton = <HTMLButtonElement>document.getElementById('restore-button');
+    const closeButton = <HTMLButtonElement>document.getElementById('close-button');
 
     closeButton.addEventListener("click", event => {
         ipc.send('closeApp');
@@ -39,4 +39,3 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 })
 
-// listener per gestione finestra
