8ec39c92588287db7302c1507a4f21a4e2fc433e
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-03-15 19:38:13 +0100

    Esaurimento risorse
    Ora le risorse possono diminurie se cliccate.
    Quando una risorse è a 0 viene scurita.

--- src/Nota.ts
+++ src/Nota.ts
@@ -64,12 +64,28 @@ export class Nota {
         const text = document.createElement('div');
         const img = document.createElement('img');
         
-        container.classList.add('container');
         img.src = `./gtfoIcons/${type}.png`;
-        img.classList.add('tool');
-        container.appendChild(img);
         text.innerText = `x${value}`;
+        
+        container.classList.add('container');
+        img.classList.add('tool');
         text.classList.add('bottom-right');
+
+        img.addEventListener('click', (event: MouseEvent) => {
+            if (event.button === 0) {
+                let number = parseInt(text.innerText.substring(1));
+
+                if (number != 0) {
+                    number--;
+                    if (number == 0) {
+                        img.classList.add('end');
+                    }
+                    text.innerText = `x${number}`;
+                } 
+            }
+        })
+
+        container.appendChild(img);
         container.appendChild(text);
 
         return container;
@@ -81,16 +97,16 @@ export class Nota {
     
         switch (event.button) {
             case 0:
+                /*
                 console.log('Left click');
                 note.classList.toggle('barred');
-    
+                */
                 break;
             case 1:
                 console.log('Middle click');
                 break;
             case 2:
                 console.log('Right click');
-                //notes.removeChild(note);
                 this.elem.remove();
                 break;
         }
