38c0a6eff7aa78572cf97b64ef9953a3991d601d
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-03-15 18:52:34 +0100

    Raggruppamento risorse
    Ora se in una riga compaiono più risorse uguali
    vengono raggruppate insieme

--- src/Nota.ts
+++ src/Nota.ts
@@ -5,31 +5,34 @@ export class Nota {
     constructor(text: string) {
         this.text = text;
         this.elem = document.createElement('span');
-        
+
         this.elem.classList.add('nota');
 
         const words = this.text.split(' ');
-        words.forEach(word => {
+        const map = words.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map()); // dizionario parola: occorrenze
+
+
+        map.forEach( (value, word) => {
             switch (word) {
                 case 'ammo':
-                    this.elem.appendChild(this.generateImage('ammo'));
+                    this.elem.appendChild(this.generateImage('ammo', value));
                     break;
                 
                 case 'medic':
                 case 'med':
                 case 'medkit':
-                    this.elem.appendChild(this.generateImage('med'));
+                    this.elem.appendChild(this.generateImage('med', value));
                     break;
                 
                 case 'tool':
-                    this.elem.appendChild(this.generateImage('tool'));
+                    this.elem.appendChild(this.generateImage('tool', value));
                     break;
                 
                 case 'disinfect':
                 case 'disinfection':
                 case 'dis':
                 case 'pack':
-                    this.elem.appendChild(this.generateImage('disinfect'));
+                    this.elem.appendChild(this.generateImage('disinfect', value));
                     break;
                 
                 default:
@@ -56,12 +59,20 @@ export class Nota {
         return this.elem;
     }
 
-    private generateImage(type: 'tool'|'ammo'|'med'|'disinfect'): HTMLImageElement {
+    private generateImage(type: 'tool'|'ammo'|'med'|'disinfect', value: number): HTMLSpanElement {
+        const container = document.createElement('span');
+        const text = document.createElement('div');
         const img = document.createElement('img');
+        
+        container.classList.add('container');
         img.src = `./gtfoIcons/${type}.png`;
         img.classList.add('tool');
-    
-        return img
+        container.appendChild(img);
+        text.innerText = `x${value}`;
+        text.classList.add('bottom-right');
+        container.appendChild(text);
+
+        return container;
     }
 
     private manageNote(note: HTMLSpanElement, event: MouseEvent): void {
