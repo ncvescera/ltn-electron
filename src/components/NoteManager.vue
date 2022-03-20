f63a177fcf5b99b6e620128060c28139b07e9368
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-03-20 23:50:57 +0100

    TextBox
    Creato componente TextBox.
    Iniziato implemento logica TextBox.
    Aggiunti altri componenti per definire la struttura del progetto

---
+++ src/components/NoteManager.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="note-box">
+        <div id="note"></div>
+    </div>
+
+    <div id="footer">    
+        <TextBox />
+    </div>
+</template>
+
+<script>
+import TextBox from './TextBox.vue'
+
+export default {
+    name: 'NoteManager',
+    components: {
+        TextBox,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
