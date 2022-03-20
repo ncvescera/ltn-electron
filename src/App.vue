2c867a9c7db19405650e0d54eb0b005f24464887
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-03-20 22:34:01 +0100

    Configurazione Vue + Webpack
    Aggiunta configurazione per caricare le immagini.
    Aggiunto file jsconfig.json (da compilare)
    Aggiunta parte della GUI (Vue)

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-    <HelloWorld message="AAAA"/>
-
-    <button @click="test()">AAA</button>
-    <p>test API: {{ test() }}</p>
+  <TitleBar title="Let Them Note" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TitleBar from './components/TitleBar.vue'
 
 export default {
-    name: 'App',
-    components: {
-        HelloWorld
-    },
-    methods: {
-        test() {
-
-            const test =  window.API.test();
-            console.log(test);
-            return test;
-        }
-    }
-    
+  name: 'App',
+  components: {
+    TitleBar
+  }
 }
 </script>
 
 <style>
-    body {
-        background-color: aqua;
-    }
+  * {
+    margin: 0;
+    padding: 0; 
+    border: 0; 
+    vertical-align: baseline;
+  }
+  
+  html {
+    box-sizing: border-box;
+  }
+  
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  html, body {
+    height: 100%; margin: 0;
+  }
+
+  body {
+    font-family: "Segoe UI", sans-serif;
+    background: #1A2933;
+    color: #FFF;
+    border: 1px solid #48545c;
+    overflow-y: hidden;
+  }
 </style>
