6e883661de05b329603191f8d1df21eefea760ee
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-03-20 23:50:57 +0100

    TextBox
    Creato componente TextBox.
    Iniziato implemento logica TextBox.
    Aggiunti altri componenti per definire la struttura del progetto

--- src/components/MainBox.vue
+++ src/components/MainBox.vue
@@ -1,18 +1,30 @@
+<!-- 
+    Box principale 
+
+    Qui ci sta un titolo e il contenitore al cui interno verranno
+    mostrate le note
+-->
 <template>
     <div id="main">
         <h1>Resources Notes 📋</h1>
+        <NoteManager />
     </div>
 </template>
 
 <script>
+import NoteManager from './NoteManager.vue'
+
 export default {
     name: 'MainBox',
+    components: {
+        NoteManager,
+    },
 }
 </script>
 
 <style scoped>
     #main {
-        margin: 10px 5% 10px 5%;
+        margin: 10px 5% 10px 5%; /* potrebbero essere tutti convertiti in px */
         width: auto;
         height: auto;
     }
---
+++ src/components/TextBox.vue
@@ -0,0 +1,34 @@
+<template>
+    <input v-model="testo" type="text" id="input" @keyup.enter="inputManager" autofocus>
+</template>
+
+<script>
+export default {
+    name: 'TextBox',
+    data() {            // ora testo è mutabile e può essere modificato
+        return {        // TODO: ricontrollare meglio col video
+            testo: '',
+        }
+    },
+    methods: {
+        inputManager(event)  {
+            this.testo = '';
+        },
+    }
+}
+</script>
+
+<style scoped>
+input {
+    width: 100%;
+    padding: 5px 5px;
+    margin: 8px 0;
+    box-sizing: border-box;
+}
+
+input:focus{
+    outline:none;
+    border-color:blue;
+    box-shadow:0 0 10px blue;
+}
+</style>
